2d503b50870356db403cb6b1c892ec714b998be5
Danny van Holten <danny@impres.nl>
2017-01-17 17:39:25 +0100

    PageModel testing

--- post/PageModel.php
+++ post/PageModel.php
@@ -8,35 +8,37 @@ namespace App\Models;
  * Class PageModel
  * @package App\Models
  */
-final class PageModel extends BasePostModel
-{
-
-	/**
-	 * PageModel constructor.
-	 *
-	 * Set all the arguments that are default for this Model
-	 */
-	public function __construct()
+if (!class_exists('PageModel')) {
+	final class PageModel extends BasePostModel
 	{
-		$this->type('page');
-	}
 
-	/**
-	 * Return a single template page.
-	 * Based on the themosis page template postmeta
-	 *
-	 * @see https://codex.wordpress.org/Class_Reference/WP_Query#Custom_Field_Parameters
-	 * @see BaseModel::meta()
-	 *
-	 * @param null $template
-	 * @return $this
-	 */
-	public static function template($template = null)
-	{
-		$instance = new static;
+		/**
+		 * PageModel constructor.
+		 *
+		 * Set all the arguments that are default for this Model
+		 */
+		public function __construct()
+		{
+			$this->type('page');
+		}
 
-		$instance->where('_wp_page_template', $template);
+		/**
+		 * Return a single template page.
+		 * Based on the themosis page template postmeta
+		 *
+		 * @see https://codex.wordpress.org/Class_Reference/WP_Query#Custom_Field_Parameters
+		 * @see BaseModel::meta()
+		 *
+		 * @param null $template
+		 * @return $this
+		 */
+		public static function template($template = null)
+		{
+			$instance = new static;
 
-		return $instance->first();
+			$instance->where('_wp_page_template', $template);
+
+			return $instance->first();
+		}
 	}
 }
